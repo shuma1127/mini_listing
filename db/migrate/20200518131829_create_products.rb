909cc7e7c59585475aabc08de9c0475877faabb6
shuma1127 <shuma.1127b@gmail.com>
2020-05-18 22:22:00 +0900

    アソシエーション

--- db/migrate/20200518131829_create_products.rb
+++ db/migrate/20200518131829_create_products.rb
@@ -1,7 +1,8 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-
+      t.string :name
+      t.integer :price
       t.timestamps
     end
   end
